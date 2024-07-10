af704a96c69415b22970629dfc5b40390bf80033
Oznak Dathomir <oznakdat@ez-b5.mag.21-school.ru>
2024-07-10 19:03:17 +0300

    created separate file for function

---
+++ s21_memset.c
@@ -0,0 +1,9 @@
+void *s21_memset(void *string, int c, s21_size_t n) {
+  unsigned char *p = (unsigned char *)string;
+  while (n > 0) {
+    *p = (unsigned char)c;
+    n--;
+    p++;
+  }
+  return string;
+}
