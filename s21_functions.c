c926e016ceaa5b3ea8e55d2df4982233a66b0f90
nspace0 <bulguchev2018@gmail.com>
2024-07-11 23:42:33 +0300

    fix

--- s21_functions.c
+++ s21_functions.c
@@ -14,14 +14,11 @@ char *s21_strerror(int errnum) {}
 
 char *s21_strpbrk(const char *string1, const char *string2) {}
 
-char *s21_strrchr(const char *string, int c) {}
-
 char *s21_strstr(const char *haystack, const char *needle) {}
 
 char *s21_strtok(char *str, const char *delim) {}
 
 // bonus quest5
-
 void *s21_insert(const char *source, const char *string,
                  s21_size_t start_index) {}
 
