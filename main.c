078b33bcd631eb8be3cb160655bcc1e9c6a9282c
nspace0 <bulguchev2018@gmail.com>
2024-07-15 16:30:30 +0300

    a

--- main.c
+++ main.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 //#include "s21_string.h"
 typedef long unsigned s21_size_t;
 
+char *s21_strrchr(const char *string, int c) {
+  c = (unsigned char)c;
+  const char *ptr = NULL;
+
+  while (*string) {
+    if (*string == c) {
+      ptr = string;
+    }
+    string++;
+  }
+  
+  if (c == '\0') {
+    return (char *)string;
+  }
+
+  return (char *)ptr;
+}
 
 int main() {
-  char str[] = "Эта программа написана в Linux Ubuntu";
-  printf("%s", strstr(str, "а"));
+  const char str[] = "https://www.tutorialspoint.com";
+  const char ch = '\0';
+  char *ret;
+
+  ret = strchr(str, ch);
+
+  printf("String after |%c| is - |%s|\n", ch, ret);
 
   return 0;
 }
