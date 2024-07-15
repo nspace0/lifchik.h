87e98e02e6d329abdd66307e6c5ef372b3880737
nspace0 <bulguchev2018@gmail.com>
2024-07-15 17:52:13 +0300

    testing

--- main.c
+++ main.c
@@ -4,33 +4,34 @@
 
 //#include "s21_string.h"
 typedef long unsigned s21_size_t;
+s21_size_t s21_strlen(const char *string) {
+  const char *end = string;
 
-char *s21_strrchr(const char *string, int c) {
-  c = (unsigned char)c;
-  const char *ptr = NULL;
-
-  while (*string) {
-    if (*string == c) {
-      ptr = string;
-    }
-    string++;
-  }
-  
-  if (c == '\0') {
-    return (char *)string;
+  while (*end != '\0') {
+    ++end;
   }
 
-  return (char *)ptr;
+  return end - string;
+}
+
+void *s21_memchr(const void *string, int c, s21_size_t n) {
+  const unsigned char *ptr = (const unsigned char *)string;
+  unsigned char ch = (unsigned char)c;
+
+  while (n--) {
+    if (*ptr == ch) return (void *)ptr;
+
+    ptr++;
+  }
+  return NULL;
 }
 
 int main() {
-  const char str[] = "https://www.tutorialspoint.com";
+  const char str[] = "Tutorialspoint";
   const char ch = '\0';
   char *ret;
-
-  ret = strchr(str, ch);
-
-  printf("String after |%c| is - |%s|\n", ch, ret);
+  ret = s21_memchr(str, ch, s21_strlen(str));
+  printf("string after |%c| is - |%s|\n", ch, ret);
 
   return 0;
 }
