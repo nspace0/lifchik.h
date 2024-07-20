099a75df8417f90a476f2e197a9a9365b2680b5b
nspace0 <bulguchev2018@gmail.com>
2024-07-20 17:45:11 +0300

    created separate file for function

---
+++ s21_memcmp.c
@@ -0,0 +1,15 @@
+#include "s21_string.h"
+
+int memcmp(const void *string1, const void *string2, s21_size_t n) {
+	const unsigned char *ptr1 = (const unsigned char*)string1;
+	const unsigned char *ptr2 = (const unsigned char*)string2;
+	
+	for(s21_size_t i = 0; i < n; i++) {
+		if(ptr[i] < ptr2[i] ) {
+			return -1;
+		} else if(ptr1[i] > ptr2[i]) {
+			return 1;
+		}
+	}
+	return 0;
+}
