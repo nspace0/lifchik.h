e26bba20dde6fb9ca5d1663dacbd1358c31715f6
nspace0 <bulguchev2018@gmail.com>
2024-07-20 17:20:27 +0300

    fix

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
+#include <errno.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 //#include "s21_string.h"
 typedef long unsigned s21_size_t;
+
 s21_size_t s21_strlen(const char *string) {
   const char *end = string;
 
@@ -14,24 +16,39 @@ s21_size_t s21_strlen(const char *string) {
   return end - string;
 }
 
-void *s21_memchr(const void *string, int c, s21_size_t n) {
-  const unsigned char *ptr = (const unsigned char *)string;
-  unsigned char ch = (unsigned char)c;
-
-  while (n--) {
-    if (*ptr == ch) return (void *)ptr;
-
-    ptr++;
+s21_size_t s21_strcspn(const char *str1, const char *str2) {
+  if (str1 == NULL || str2 == NULL) {
+    errno = EINVAL;
+    return 0;
   }
-  return NULL;
+
+  s21_size_t result = 0;
+  int flags[256] = {0};
+
+  for (const char *p = str2; *p != '\0'; p++) {
+    flags[(unsigned char)*p] = 1;
+  }
+
+  for (const char *p = str1; *p != '\0'; p++) {
+    if (flags[(unsigned char)*p]) {
+      return result;
+    }
+    result++;
+  }
+
+  return result;
 }
 
-int main() {
-  const char str[] = "Tutorialspoint";
-  const char ch = '\0';
-  char *ret;
-  ret = s21_memchr(str, ch, s21_strlen(str));
-  printf("string after |%c| is - |%s|\n", ch, ret);
 
+
+int main() {
+  int len;
+  // Intializing string(Unmatched Characters)
+  const char str1[] = "Tutorialspoint";
+  const char str2[] = "ETextbook";
+
+  len = s21_strcspn(str1, str2);
+
+  printf("First matched character is at %d\n", len);
   return 0;
 }
