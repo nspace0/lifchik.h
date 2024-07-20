8b00b96c1309d131c330a2c2bf08231d46e382cd
nspace0 <bulguchev2018@gmail.com>
2024-07-20 17:14:28 +0300

    fixed

--- s21_functions.c
+++ s21_functions.c
@@ -4,8 +4,6 @@
 
 // void *s21_memcpy(void *destination, const void *source, s21_size_t n) {}
 
-s21_size_t s21_strcspn(const char *str1, const char *str2) {}
-
 char *s21_strerror(int errnum) {}
 
 char *s21_strpbrk(const char *string1, const char *string2) {}
