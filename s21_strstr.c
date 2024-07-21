d41f88fa02d24ca3d1ea7e5dcf480b877a8f8455
nspace0 <bulguchev2018@gmail.com>
2024-07-21 23:17:26 +0300

    fix

--- s21_strstr.c
+++ s21_strstr.c
@@ -1,8 +1,8 @@
 #include "s21_string.h"
 
-char *s21_strstr(const char* haystack, const char* needle) {
-  size_t s1len = strlen(haystack);
-  size_t s2len = strlen(needle);
+char* s21_strstr(const char* haystack, const char* needle) {
+  size_t s1len = s21_strlen(haystack);
+  size_t s2len = s21_strlen(needle);
 
   if (s2len > s1len) return s21_NULL;
 
