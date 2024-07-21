642089e3c89c930547efd49ecb07ca68d4c24ec4
nspace0 <bulguchev2018@gmail.com>
2024-07-21 23:49:55 +0300

    fixed

--- s21_functions.c
+++ s21_functions.c
@@ -1,14 +1,6 @@
 #include "s21_string.h"
 
-// int s21_memcmp(const void *string1, const void *string2, s21_size_t n) {}
-
-// void *s21_memcpy(void *destination, const void *source, s21_size_t n) {}
-
 char *s21_strerror(int errnum) {}
-
-char *s21_strpbrk(const char *string1, const char *string2) {}
-
-char *s21_strstr(const char *haystack, const char *needle) {}
 
 char *s21_strtok(char *str, const char *delim) {}
 
